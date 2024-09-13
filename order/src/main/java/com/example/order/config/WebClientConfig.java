2af89b4e479f5443d58dd79d9169f1e79596353f
Godagesasindu <godagesasindu@gmail.com>
2024-09-14 01:20:33 +0530

    service discovery with eureka server

--- order/src/main/java/com/example/order/config/WebClientConfig.java
+++ order/src/main/java/com/example/order/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.example.order.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,17 +8,18 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
     @Bean
-    public WebClient webClient() {
-        return WebClient.builder().build();
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 
     @Bean
     public WebClient inventoryWebClient() {
-        return WebClient.builder().baseUrl("http://localhost:8080/api/ABC").build();
+        return webClientBuilder().baseUrl("http://inventory/api/ABC").build();
     }
 
     @Bean
     public WebClient productWebClient() {
-        return WebClient.builder().baseUrl("http://localhost:8082/api/ABC").build();
+        return webClientBuilder().baseUrl("http://product/api/ABC").build();
     }
 }
